d371b803b9b557fdd6a349df1275362ec8221598
alejandro <alejandro@Parsers-MacBook-Air.local>
2023-02-15 10:51:39 -0500

    refactor appDelegate, removed google token

--- LocationGoogleApp/AppDelegate.swift
+++ LocationGoogleApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyB9fImK-NOqu1ytPzC0ezpXFpSoid7nKDQ")
+        GMSServices.provideAPIKey("token")
         // Override point for customization after application launch.
         return true
     }
